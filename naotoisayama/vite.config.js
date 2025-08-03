222cf1d5e816bef910b312a924a4b59353fb0ed8
Naoto <naotojacob@gmail.com>
2025-08-03 01:25:09 -0600

    styling done on desktop aboutme.html

--- naotoisayama/vite.config.js
+++ naotoisayama/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
       input: {
         main: 'index.html',
         aboutme: 'aboutme.html',
+        mywork: 'mywork.html',
         // Add other pages as needed
       }
     }
